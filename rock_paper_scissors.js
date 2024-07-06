b76b5f765a76092bcf9727bc1bb74b859c92940a
Harman <haskhatkar@gmail.com>
2024-07-06 16:10:00 -0700

    Finished Project

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,62 +1,82 @@
-const getComputerHand = () => {
-    choices = ["rock", "paper", "scissors"];
-    randomInt = Math.floor(Math.random()*choices.length);
-    return choices[randomInt];
+let playerScore = 0
+let computerScore = 0
+
+const buttons = document.querySelectorAll('.element-button')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        playRound(button.value)
+    })
+})
+
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
 }
 
-console.log(getComputerHand());
+function computerPlay() {
+    const choices = ['rock', 'paper', 'scissors'];
+    let choicesIndex = Math.floor(Math.random() * choices.length);
 
-
-const getHumanHand = () => {
-    const ask = prompt("Choose rock paper or scissors").toLowerCase();
-    if (["rock", "paper", "scissors"].includes(ask)) {
-        return ask;
-    } else {
-        alert("Wrong! Choose rock paper or scissors!");
-        return getHumanHand();
-    }
+    return choices[choicesIndex];
 };
 
-console.log(getHumanHand());
+console.log(computerPlay());
 
-const playGame = () => {
-    let humanScore = 0;
-    let computerScore = 0;
 
-    const roundOne = (humanHand, computerHand) => {
-        humanHand = humanHand.toLowerCase();
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
+    let result = ""
 
-        if (humanHand === computerHand) {
-            console.log(`It's a tie! Both played ${humanHand}`);
-        } else if (
-            (humanHand === "rock" && computerHand === "scissors") ||
-            (humanHand === "paper" && computerHand === "rock") ||
-            (humanHand === "scissors" && computerHand === "paper"))
-        {
-            console.log(`You win! Played: ${humanHand} and Computer played ${computerHand}`);
-            humanScore++;
-        } else {
-            console.log(`You lose! Played: ${humanHand} and Computer played ${computerHand}`);
-            computerScore++;
+    if (playerSelection == computerSelection) {
+        result = '<br>You both picked ' + playerSelection
+    }
+
+    else if ((playerSelection == 'rock' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection = 'scissors' && computerSelection == 'paper')) {
+            playerScore += 1
+            result = '<br><b>You win: </b><br>' + playerSelection + ' beats ' + computerSelection
+
+                if (playerScore == 5) {
+                    result += '<br><br>Player wins game. Reload page to play again.'
+                    disableButtons()
+                }
+    }
+
+    else {
+        computerScore += 1
+        result = '<br><b>Defeat:</b><br>' + computerSelection + ' beats ' + playerSelection
+
+            if (computerScore == 5) {
+                result += '<br><br>Computer wins game. Reload the page to play again.'
+                disableButtons()
+            }
         }
 
-        console.log(`Current score is ${humanScore} for Player and ${computerScore} for Computer`);
-    }
-    for (i=0; i<7; i++) {
-        let humanCount = getHumanHand();
-        let computerCount = getComputerHand();
-        roundOne(humanCount, computerCount);
-    }
+        updateChoices(playerSelection, computerSelection);
 
+        document.getElementById('result').innerHTML = result
+        document.getElementById('result-playerscore').innerHTML = `<b>Player:</b><br> ${playerScore}`;
+        document.getElementById('result-computerscore').innerHTML = `<b>Computer:</b><br>
+    ${computerScore}`;
 
-    if (humanScore > computerScore) {
-        console.log(`You won the game!!! With ${humanScore} points vs ${computerScore} points for computer`)
-    } else if (humanScore < computerScore) {
-        console.log(`You lost the game!!! With ${humanScore} points vs ${computerScore} points for Computer`)
-    } else {
-        console.log("It's a tie game!!!!");
-    }
+}
 
-};
-
-playGame();
+function updateChoices(playerSelection, computerSelection) {
+    // create variables to link to our HTML elements
+    const playerElement = document.getElementById("player-element"); 
+    const computerElement = document.getElementById("computer-element"); 
+    // assigns classList property to our variables, than adds 'active' class to our classList 
+    playerElement.classList.add("active");
+    computerElement.classList.add("active"); 
+    
+    // creates a second set of variables to pull our button.value
+    const playerElementClassName = `fa-${playerSelection}`
+    const computerElementClassName = `fa-${computerSelection}`
+    
+    playerElement.classList = `fas ${playerElementClassName} active`;
+    computerElement.classList = `fas ${computerElementClassName} active`;
+    
+  }
